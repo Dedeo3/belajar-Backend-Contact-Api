ffbc90e721fa6a30a7a38ce4e2710b7d466f57eb
Aryo-Wibisono <yoannes.deo@ti.ukdw.ac.id>
2024-11-25 01:51:01 +0700

    create handler middleware for eror situation

---
+++ src/middleware/error-middleware.js
@@ -0,0 +1,21 @@
+import {ResponseEror} from "../error/response-eror.js";
+import {ValidationError} from "joi";
+
+const errorMiddleware = async (err, req, res, next) => {
+    if (!err){
+        next()
+        return
+    }
+
+    if (err instanceof ResponseEror) {
+        res.status(err.status).json({
+            error: err.message}).end();
+    }
+    else {
+        res.status(500).json({
+            error: err.message
+        }).end()
+    }
+}
+
+export {errorMiddleware}
